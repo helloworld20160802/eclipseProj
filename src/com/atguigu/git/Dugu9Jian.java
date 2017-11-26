eea1ba3e506cbf658975fc7b4a490dc7368bbe2e
wuzhuang <wzstart@126.com>
2017-11-26 11:19:27 +0800

    岳不群进行修改文件了

--- src/com/atguigu/git/Dugu9Jian.java
+++ src/com/atguigu/git/Dugu9Jian.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class Dugu9Jian {
 	public static void main(String[] args) {
-		System.out.println("hello wz");
+		System.out.println("hello --wz--");
+		System.out.println("我是岳不群,进行修改文件!");
 	}
 }
